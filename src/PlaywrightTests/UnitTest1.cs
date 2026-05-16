cc5f4bb280f8d427a372ef684201ba8172b79aea
katalie <kataliesun@gmail.com>
2026-05-16 21:24:49 +0000

    init commit for playright

---
+++ src/PlaywrightTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace PlaywrightTests;
+
+public class UnitTest1: PageTest
+{
+    [Fact]
+    public async Task Test1()
+    {
+        await Page.GotoAsync("https://www.bankofcanada.ca/");
+    }
+}
